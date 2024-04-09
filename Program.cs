bc608a3a00b324410aff19576864b8e64de55ec4
Xtaze <Xtaze@DESKTOP-2DTRJ02>
2024-04-10 00:48:58 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_простого_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool simple = true;
+            int number;
+            int a = 2;
+            Console.WriteLine("Введите число: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (a <= number - 1)
+            {
+                if (number % a == 0)
+                {
+                    simple = false;
+                }
+                a++;
+            }
+            if (simple)
+            {
+                Console.WriteLine("Данное число является простым");
+            }
+            else
+            {
+                Console.WriteLine("Данное число не является простым");
+            }
+            Console.ReadKey();
+        }
+    }
+}
